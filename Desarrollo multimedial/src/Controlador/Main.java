e09ba0b43bafeb617ccfafc6c8691d61e098b018
Diego <diego.mateus@javeriana.edu.co>
2020-11-15 14:16:00 -0500

    Aproximación a Video

--- Desarrollo multimedial/src/Controlador/Main.java
+++ Desarrollo multimedial/src/Controlador/Main.java
@@ -1,41 +1,97 @@
 package Controlador;
 
+import java.io.IOException;
+import java.net.URL;
+
+import javax.sound.sampled.LineUnavailableException;
+
 import Modelo.*;
 import Vista.Vista;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		Vista vista = new Vista();
-		Imagen imagen = new Imagen();
+		//Imagen imagen = new Imagen();
 		Video video = new Video();
 		Audio audio = new Audio("null");
 		int opcion = 0;
-		
+		boolean primeraVezAudio = true;
+		URL myURL = new URL("http://example.com/");
 		while(opcion != 5) {
 			
 			opcion = vista.menu();
-			String ruta;
+			String rutaAudio;
+			String rutaVideo;
+			int opcionAudio = 0;
+			
 			
 			switch(opcion) {
 				case 1:
-					ruta = vista.pedirRuta();
-					video.cargar(ruta);
+					rutaVideo = vista.pedirRutaVideo();
+					video.setRuta(rutaVideo);
+					video.main(args);
 					break;
 				case 2:
-					ruta = vista.pedirRuta();
-					audio.setFileName(ruta);
+					rutaAudio = vista.pedirRutaAudio();
+					audio.setFileName(rutaAudio);
 					audio.cargar();
+					break;
+				case 3:
+				
+					
+				case 4:
+					if(audio.fileName.equals("null")) {
+						System.out.println("Cargue un archivo primero");
+					}
+					else {
+						
+						try {
+							if(primeraVezAudio == true) {
+								audio.clip.open(audio.audioIn);
+								primeraVezAudio = false;
+							}
+							
+							
+							do {
+								opcionAudio = vista.comandosAudio();
+								switch(opcionAudio) {
+									case 1:
+										audio.reproducir();
+										break;
+									case 2:
+										audio.pausar();
+										break;
+									case 3:
+										audio.reanudar();
+										break;
+									case 4:
+										audio.pausar();
+										audio.clip.setMicrosecondPosition(0);
+										break;
+								}
+								
+							}while(opcionAudio != 4);
+						
+						}catch (LineUnavailableException e) {
+							
+							e.printStackTrace();
+						}	
+					}
+					
 					break;
 				default:
 					break;
-			}
-			
+			}		
 		}
-		
-		
-
 	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- Desarrollo multimedial/src/Modelo/Video.java
+++ Desarrollo multimedial/src/Modelo/Video.java
@@ -1,8 +1,46 @@
 package Modelo;
 
-public class Video {
-	public void cargar(String ruta) {
-		System.out.println("Se metio cideo");
+import java.io.File;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+
+public class Video extends Application{
+	
+	public String ruta;
+	
+	public String getRuta() {
+		return ruta;
 	}
 
+	public void setRuta(String ruta) {
+		this.ruta = ruta;
+	}
+
+	public void start(Stage stage) throws Exception {
+		System.out.println(this.ruta);
+        final String NOMBRE_ARCHIVO = "video.mp4";
+        File archivo = new File(this.ruta);
+        
+        Media video = new Media(archivo.toURI().toString());
+        MediaPlayer reproductor = new MediaPlayer(video);
+        MediaView ventana = new MediaView(reproductor);
+        reproductor.setAutoPlay(true);
+        
+        Group escenario = new Group();
+        escenario.getChildren().add(ventana);
+        Scene escena = new Scene(escenario, 500, 400);
+        stage.setScene(escena);
+        stage.setTitle("Reproducción Vídeo");
+        stage.show();
+    }
+	
+	public void main(String[] args) throws Exception {
+        launch(args);
+    }
+	
 }
